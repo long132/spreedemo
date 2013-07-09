a31f00aa1c1eee80a6fd4da469636f312c245469
Michael <michael@michael-VirtualBox.(none)>
2013-07-09 15:01:18 +0800

    deploying rails 4 spree to heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystore::Application.config.secret_key_base = '87da582e777acb6ce403d96080e02c843af4b46ce7fc6bc5d5bb5ecde7a330743f38cce6240a6f4047a78620f0dd8b244f77f11ef2877d004701ebe1bb06cf00'
